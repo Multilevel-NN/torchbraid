77f7d763b3730e71f71261ef6e8078c6d0c0afbd
Eric C. Cyr <eccyr@sandia.gov>
2020-02-01 20:05:26 -0700

    Updating for neew xbraid

--- python/setup.py
+++ python/setup.py
@@ -5,20 +5,39 @@ import numpy
 import os
 
 module_name = 'torchbraid'
+on_mac = True
 
-braid_dir = '/home/eccyr/Packages/xbraid/braid'
+if not on_mac:
 
-os.environ["CC"] = 'mpicc'
-os.environ["LDSHARED"] = 'mpicc -shared'
+  braid_dir = '/home/eccyr/Packages/xbraid/braid'
+  braid_dir = '/Users/eccyr/Packages/xbraid/braid'
+  
+  os.environ["CC"] = 'mpicc'
+  os.environ["LDSHARED"] = 'mpicc -shared'
+  
+  torchbraid_ext = Extension(
+  		name=module_name,
+  		sources=["%s.pyx" % module_name],
+  		libraries=["braid"],
+  		library_dirs=[braid_dir],
+  		include_dirs=[braid_dir,numpy.get_include()],
+  		extra_compile_args=['-fPIC']
+  )
 
-torchbraid_ext = Extension(
-    name=module_name,
-    sources=["%s.pyx" % module_name],
-    libraries=["braid"],
-    library_dirs=[braid_dir],
-    include_dirs=[braid_dir,numpy.get_include()],
-    extra_compile_args=['-fPIC']
-)
+else:
+
+  braid_dir = '/Users/eccyr/Packages/xbraid/braid'
+  
+  os.environ["CC"] = 'mpicc'
+  
+  torchbraid_ext = Extension(
+  		name=module_name,
+  		sources=["%s.pyx" % module_name],
+  		libraries=["braid"],
+  		library_dirs=[braid_dir],
+  		include_dirs=[braid_dir,numpy.get_include()],
+  )
+# end else
 
 setup(name=module_name,
       ext_modules=cythonize([torchbraid_ext],
