12e0897d02d096f54b7b90d38d1b477270b343c7
Eric C. Cyr <eccyr@sandia.gov>
2020-03-19 17:43:28 -0600

    Fixing some formatting

--- python/test_bwd.py
+++ python/test_bwd.py
@@ -86,61 +86,6 @@ class TestTorchBraid(unittest.TestCase):
     print('----------------------------')
   # end test_linearNet
 
-  def atest_gradient(self):
-    dim = 2
-    basic_block = lambda: ReLUBlock(dim)
-
-    x0 = torch.randn(5,dim) # forward initial cond
-    w0 = torch.randn(5,dim) # adjoint initial cond
-
-    Tf = 2.0
-    num_steps = 10
-
-    # run forward/backward propgation
-
-    # this is the torchbraid class being tested 
-    #######################################
-    m = torchbraid.Model(MPI.COMM_WORLD,basic_block,num_steps,Tf,max_levels=1,max_iters=1)
-    m.setPrintLevel(0)
-
-    # this is the reference torch "solution"
-    #######################################
-    dt = Tf/num_steps
-    ode_layers = [ODEBlock(l,dt) for l in m.local_layers.children()]
-    f = torch.nn.Sequential(*ode_layers)
-
-    # propogation with torch
-    #######################################
-    xf = x0.clone()
-    xf.requires_grad = True
-    
-    f.zero_grad()
-    for p in f.parameters():
-      print(p.grad)
-
-    wf = f(xf)
-    wf.backward(w0)
-
-    print('\ntorch grad parameters')
-    for p in f.parameters():
-      print(p.grad)
-
-    # propogation with torchbraid
-    #######################################
-    xm = x0.clone()
-    xm.requires_grad = True
-
-    m.zero_grad()
-    for p in m.parameters():
-      print(p.grad)
-
-    wm = m(xm)
-    wm.backward(w0)
-
-    print('\ntorchbraid grad parameters')
-    for p in m.parameters():
-      print(p.grad)
-
   def backForwardProp(self,dim, basic_block,x0,w0,max_levels=1,max_iters=1):
     Tf = 2.0
     num_steps = 10
@@ -190,7 +135,7 @@ class TestTorchBraid(unittest.TestCase):
       self.assertTrue((torch.norm(xm.grad-xf.grad)/torch.norm(xf.grad))<=1e-16)
 
       for pf,pm in zip(f.parameters(),m.parameters()):
-        print('p grad error = %.6e' % (torch.norm(pf.grad-pm.grad)), pf.grad.shape)
+        print('p grad error = %.6e (norm=%.6e, shape=%s)' % (torch.norm(pf.grad-pm.grad), torch.norm(pf.grad), pf.grad.shape))
         self.assertTrue(torch.norm(pf.grad-pm.grad)<=1e-16)
         
   # test_forwardPropSerial
