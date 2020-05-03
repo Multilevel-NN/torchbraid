59e8103d1ebf0970b4b0eed65aca436b19a979c6
Eric C. Cyr <eccyr@sandia.gov>
2020-05-03 14:08:25 -0600

    Adding more timings to try to improve performance

--- torchbraid/torchbraid_callbacks.pyx
+++ torchbraid/torchbraid_callbacks.pyx
@@ -17,59 +17,69 @@ cdef int my_access(braid_App app,braid_Vector u,braid_AccessStatus status):
 
   cdef double t
 
-  # Create Numpy wrapper around u.v
-  ten_u = <object> u
+  with pyApp.timer("my_access"):
+    # Create Numpy wrapper around u.v
+    ten_u = <object> u
 
-  braid_AccessStatusGetT(status, &t)
+    braid_AccessStatusGetT(status, &t)
 
-  pyApp.access(t,ten_u)
+    pyApp.access(t,ten_u)
   return 0
 
 cdef int my_step(braid_App app, braid_Vector ustop, braid_Vector fstop, braid_Vector vec_u, braid_StepStatus status):
   pyApp = <object> app
-  u =  <object> vec_u
 
   cdef double tstart
   cdef double tstop
   cdef int level
-  tstart = 0.0
-  tstop = 5.0
-  level = -1
-  braid_StepStatusGetTstartTstop(status, &tstart, &tstop)
-  braid_StepStatusGetLevel(status, &level)
 
-  temp = pyApp.eval(u,tstart,tstop,level)
-  u.tensor().copy_(temp.tensor())
+  with pyApp.timer("my_step"):
+
+    tstart = 0.0
+    tstop = 5.0
+    level = -1
+    braid_StepStatusGetTstartTstop(status, &tstart, &tstop)
+    braid_StepStatusGetLevel(status, &level)
+
+    u =  <object> vec_u
+    temp = pyApp.eval(u,tstart,tstop,level)
+
+    u.tensor().copy_(temp.tensor())
 
   return 0
 # end my_access
 
 cdef int my_init(braid_App app, double t, braid_Vector *u_ptr):
   pyApp = <object> app
-  u_mem = pyApp.buildInit(t)
-  Py_INCREF(u_mem) # why do we need this?
-  u_ptr[0] = <braid_Vector> u_mem
+  with pyApp.timer("my_init"):
+    u_mem = pyApp.buildInit(t)
+    Py_INCREF(u_mem) # why do we need this?
+    u_ptr[0] = <braid_Vector> u_mem
+
   return 0
 
 cdef int my_free(braid_App app, braid_Vector u):
-  # Cast u as a PyBraid_Vector
-  pyU = <object> u
-  # Decrement the smart pointer
-  Py_DECREF(pyU) 
-  del pyU
+  pyApp = <object> app
+  with pyApp.timer("my_free"):
+
+    # Cast u as a PyBraid_Vector
+    pyU = <object> u
+    # Decrement the smart pointer
+    Py_DECREF(pyU) 
+    del pyU
   return 0
 
 cdef int my_sum(braid_App app, double alpha, braid_Vector x, double beta, braid_Vector y):
   # This routine cna be made faster by using the pyTorch tensor operations
   # My initial attempt at this failed however
 
-  py_app = <object>app
+  pyApp = <object>app
 
   cdef np.ndarray[float,ndim=1] np_X
   cdef np.ndarray[float,ndim=1] np_Y
   cdef int sz
 
-  with py_app.timer("my_sum"):
+  with pyApp.timer("my_sum"):
     # Cast x and y as a PyBraid_Vector
     np_X = (<object> x).tensor().numpy().ravel()
     np_Y = (<object> y).tensor().numpy().ravel()
@@ -82,28 +92,34 @@ cdef int my_sum(braid_App app, double alpha, braid_Vector x, double beta, braid_
   return 0
 
 cdef int my_clone(braid_App app, braid_Vector u, braid_Vector *v_ptr):
-  ten_U = <object> u 
-  v_mem = ten_U.clone()
-  Py_INCREF(v_mem) # why do we need this?
-  v_ptr[0] = <braid_Vector> v_mem
+  pyApp = <object> app
+  with pyApp.timer("my_clone"):
+    ten_U = <object> u 
+    v_mem = ten_U.clone()
+    Py_INCREF(v_mem) # why do we need this?
+    v_ptr[0] = <braid_Vector> v_mem
 
   return 0
 
 cdef int my_norm(braid_App app, braid_Vector u, double *norm_ptr):
-  # Compute norm 
-  ten_U = (<object> u).tensor()
-  norm_ptr[0] = torch.norm(ten_U)
+  pyApp = <object> app
+  with pyApp.timer("my_norm"):
+    # Compute norm 
+    ten_U = (<object> u).tensor()
+    norm_ptr[0] = torch.norm(ten_U)
 
   return 0
 
 cdef int my_bufsize(braid_App app, int *size_ptr, braid_BufferStatus status):
   pyApp = <object> app
-  cdef int cnt = pyApp.x0.tensor().size().numel()
+  cdef int cnt 
+  with pyApp.timer("my_bufsize"):
+    cnt = pyApp.x0.tensor().size().numel()
 
-  # Note size_ptr is an integer array of size 1, and we index in at location [0]
-  # the int size encodes the level
-  size_ptr[0] = sizeof(float)*cnt + sizeof(float) + sizeof(int)
-              # vector                 time             level
+    # Note size_ptr is an integer array of size 1, and we index in at location [0]
+    # the int size encodes the level
+    size_ptr[0] = sizeof(float)*cnt + sizeof(float) + sizeof(int)
+                   # vector                 time             level
 
   return 0
 
@@ -116,8 +132,8 @@ cdef int my_bufpack(braid_App app, braid_Vector u, void *buffer,braid_BufferStat
   cdef int sz
   cdef view.array my_buf 
 
-  py_app = <object>app
-  with py_app.timer("my_bufpack"):
+  pyApp = <object>app
+  with pyApp.timer("my_bufpack"):
     # Cast u as a PyBraid_Vector
     ten_U = (<object> u).tensor()
     np_U  = ten_U.numpy().ravel() # ravel provides a flatten accessor to the array
@@ -133,27 +149,30 @@ cdef int my_bufpack(braid_App app, braid_Vector u, void *buffer,braid_BufferStat
   return 0
 
 cdef int my_bufunpack(braid_App app, void *buffer, braid_Vector *u_ptr,braid_BufferStatus status):
-  py_app = <object>app
+  pyApp = <object>app
 
   cdef int * ibuffer = <int *> buffer
   cdef float * fbuffer = <float *>(buffer+sizeof(int))
-  cdef braid_Vector c_x = <braid_Vector> py_app.x0
   cdef np.ndarray[float,ndim=1] np_U
   cdef int sz
   cdef view.array my_buf 
 
-  with py_app.timer("my_bufunpack"):
+  with pyApp.timer("my_bufunpack"):
   
-    my_clone(app,c_x,u_ptr)
+    # allocate memory
+    u_obj = pyApp.x0.clone()
+    Py_INCREF(u_obj) # why do we need this?
+    u_ptr[0] = <braid_Vector> u_obj 
   
-    (<object> u_ptr[0]).level_ = ibuffer[0]
-    (<object> u_ptr[0]).setTime(fbuffer[0])
+    u_obj.level_ = ibuffer[0]
+    u_obj.setTime(fbuffer[0])
 
-    ten_U = (<object> u_ptr[0]).tensor()
+    ten_U = u_obj.tensor()
     np_U = ten_U.numpy().ravel() # ravel provides a flatten accessor to the array
-    sz = len(np_U)
   
     # this is almost certainly slow
+    sz = len(np_U)
     my_buf = <float[:sz]> (fbuffer+1)
     np_U[:] = my_buf
+
   return 0
