14f7a0c2a5397d854649334086f60fc903bb8212
Eric C. Cyr <eccyr@sandia.gov>
2020-06-09 19:33:29 -0600

    Updating copyright

--- examples/mnist/main.py
+++ examples/mnist/main.py
@@ -2,10 +2,7 @@
 # ************************************************************************
 # 
 #                        Torchbraid v. 0.1
-#              Copyright (2014) Sandia Corporation
 # 
-# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-# the U.S. Government retains certain rights in this software.
 # Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC 
 # (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. 
 # Government retains certain rights in this software.
@@ -27,17 +24,33 @@
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  NOTICE:
+#
+# For five (5) years from  the United States Government is granted 
+# for itself and others acting on its behalf a paid-up, nonexclusive, 
+# irrevocable worldwide license in this data to reproduce, prepare derivative 
+# work, and perform publicly and display publicly, by or on behalf of the 
+# Government. There is provision for the possible extension of the term of
+# this license. Subsequent to that period or any extension granted, the 
+# United States Government is granted for itself and others acting on its
+# behalf a paid-up, nonexclusive, irrevocable worldwide license in this data
+# to reproduce, prepare derivative works, distribute copies to the public,
+# perform publicly and display publicly, and to permit others to do so. The
+# specific term of the license can be identified by inquiry made to National
+# Technology and Engineering Solutions of Sandia, LLC or DOE.
+#
+# NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT OF 
+# ENERGY, NOR NATIONAL TECHNOLOGY AND ENGINEERING SOLUTIONS OF SANDIA, LLC, 
+# NOR ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+# ASSUMES ANY LEGAL RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR 
+# USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR PROCESS DISCLOSED, 
+# OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+# 
+# Any licensee of this software has the obligation and responsibility to 
+# abide by the applicable export control laws, regulations, and general 
+# prohibitions relating to the export of technical data. Failure to obtain 
+# an export control license or other authority from the Government may 
+# result in criminal liability under U.S. laws.
 #
 # Questions? Contact Eric C. Cyr (eccyr@sandia.gov)
 # 
