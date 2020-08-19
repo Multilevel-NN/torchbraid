8ba12d03cfc70da653f62f0b7ef9923a9fd12b4c
Eric C. Cyr <eccyr@sandia.gov>
2020-08-19 09:40:50 -0600

    Initial commit of nested iteration

---
+++ examples/cifar10/main_nested_iter.py
@@ -0,0 +1,364 @@
+#@HEADER
+# ************************************************************************
+# 
+#                        Torchbraid v. 0.1
+# 
+# Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC 
+# (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. 
+# Government retains certain rights in this software.
+# 
+# Torchbraid is licensed under 3-clause BSD terms of use:
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name National Technology & Engineering Solutions of Sandia, 
+# LLC nor the names of the contributors may be used to endorse or promote 
+# products derived from this software without specific prior written permission.
+# 
+# Questions? Contact Eric C. Cyr (eccyr@sandia.gov)
+# 
+# ************************************************************************
+#@HEADER
+
+# some helpful examples
+# 
+# BATCH_SIZE=50
+# STEPS=12
+# CHANNELS=8
+
+# IN SERIAL
+# python  main.py --steps ${STEPS} --channels ${CHANNELS} --batch-size ${BATCH_SIZE} --log-interval 100 --epochs 20 # 2>&1 | tee serial.out
+# mpirun -n 4 python  main.py --steps ${STEPS} --channels ${CHANNELS} --batch-size ${BATCH_SIZE} --log-interval 100 --epochs 20 # 2>&1 | tee serial.out
+
+from __future__ import print_function
+import sys
+import argparse
+import statistics as stats
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchbraid
+import torchbraid.utils
+
+from torchvision import datasets, transforms
+
+from timeit import default_timer as timer
+
+from mpi4py import MPI
+
+def root_print(rank,s):
+  if rank==0:
+    print(s)
+
+class OpenLayer(nn.Module):
+  def __init__(self,channels):
+    super(OpenLayer, self).__init__()
+    ker_width = 3
+    self.conv = nn.Conv2d(3,channels,ker_width,padding=1)
+
+  def forward(self, x):
+    return F.relu(self.conv(x))
+# end layer
+
+class CloseLayer(nn.Module):
+  def __init__(self,channels):
+    super(CloseLayer, self).__init__()
+    ker_width = 3
+
+    self.fc1 = nn.Linear(channels*32*32, 128)
+    self.fc2 = nn.Linear(128, 10)
+
+  def forward(self, x):
+    x = torch.flatten(x, 1)
+    x = self.fc1(x)
+    x = F.relu(x)
+    x = self.fc2(x)
+    output = F.log_softmax(x, dim=1)
+    return output
+# end layer
+
+class StepLayer(nn.Module):
+  def __init__(self,channels):
+    super(StepLayer, self).__init__()
+    ker_width = 3
+    self.conv1 = nn.Conv2d(channels,channels,ker_width,padding=1)
+    self.bn1 = nn.BatchNorm2d(channels)
+    self.conv2 = nn.Conv2d(channels,channels,ker_width,padding=1)
+    self.bn2 = nn.BatchNorm2d(channels)
+
+  def forward(self, x):
+    return F.relu(self.bn2(self.conv2(F.relu(self.bn1(self.conv1(x))))))
+# end layer
+
+class SerialNet(nn.Module):
+  def __init__(self,channels=12,local_steps=8,Tf=1.0):
+    super(SerialNet, self).__init__()
+
+    step_layer = lambda: StepLayer(channels)
+    
+    self.open_nn = OpenLayer(channels)
+    self.parallel_nn = torchbraid.LayerParallel(MPI.COMM_WORLD,step_layer,local_steps,Tf,max_levels=1,max_iters=1)
+    self.parallel_nn.setPrintLevel(0)
+    
+    self.serial_nn   = self.parallel_nn.buildSequentialOnRoot()
+    self.close_nn = CloseLayer(channels)
+ 
+  def forward(self, x):
+    x = self.open_nn(x)
+    x = self.serial_nn(x)
+    x = self.close_nn(x)
+    return x
+# end SerialNet 
+
+class ParallelNet(nn.Module):
+  def __init__(self,channels=12,local_steps=8,Tf=1.0,max_levels=1,max_iters=1,print_level=0):
+    super(ParallelNet, self).__init__()
+
+    step_layer = lambda: StepLayer(channels)
+
+    self.parallel_nn = torchbraid.LayerParallel(MPI.COMM_WORLD,step_layer,local_steps,Tf,max_levels=max_levels,max_iters=max_iters)
+    self.parallel_nn.setPrintLevel(print_level)
+    self.parallel_nn.setCFactor(4)
+    self.parallel_nn.setSkipDowncycle(True)
+    self.parallel_nn.setNumRelax(1)         # FCF elsewehre
+    self.parallel_nn.setNumRelax(0,level=0) # F-Relaxation on the fine grid
+
+    # this object ensures that only the LayerParallel code runs on ranks!=0
+    compose = self.compose = self.parallel_nn.comp_op()
+    
+    # by passing this through 'compose' (mean composition: e.g. OpenLayer o channels) 
+    # on processors not equal to 0, these will be None (there are no parameters to train there)
+    self.open_nn = compose(OpenLayer,channels)
+    self.close_nn = compose(CloseLayer,channels)
+ 
+  def forward(self, x):
+    # by passing this through 'o' (mean composition: e.g. self.open_nn o x) 
+    # this makes sure this is run on only processor 0
+
+    x = self.compose(self.open_nn,x)
+    x = self.parallel_nn(x)
+    x = self.compose(self.close_nn,x)
+
+    return x
+# end ParallelNet 
+
+def train(rank, args, model, train_loader, optimizer, epoch,compose):
+  model.train()
+  criterion = nn.CrossEntropyLoss()
+  total_time = 0.0
+  for batch_idx, (data, target) in enumerate(train_loader):
+    start_time = timer()
+    optimizer.zero_grad()
+    output = model(data)
+    loss = compose(criterion,output,target)
+    loss.backward()
+    stop_time = timer()
+    optimizer.step()
+
+    total_time += stop_time-start_time
+    if batch_idx % args.log_interval == 0:
+      root_print(rank,'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime Per Batch {:.6f}'.format(
+          epoch, batch_idx * len(data), len(train_loader.dataset),
+          100. * batch_idx / len(train_loader), loss.item(),total_time/(batch_idx+1.0)))
+
+  root_print(rank,'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime Per Batch {:.6f}'.format(
+    epoch, (batch_idx+1) * len(data), len(train_loader.dataset),
+    100. * (batch_idx+1) / len(train_loader), loss.item(),total_time/(batch_idx+1.0)))
+
+
+def test(rank, model, test_loader,compose):
+  model.eval()
+  test_loss = 0
+  correct = 0
+  criterion = nn.CrossEntropyLoss()
+  with torch.no_grad():
+    for data, target in test_loader:
+      data, target = data, target
+      output = model(data)
+      test_loss += compose(criterion,output,target).item()
+       
+      output = MPI.COMM_WORLD.bcast(output,root=0)
+      pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+      correct += pred.eq(target.view_as(pred)).sum().item()
+
+  test_loss /= len(test_loader.dataset)
+
+  root_print(rank,'\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
+
+def compute_levels(num_steps,min_coarse_size,cfactor): 
+  from math import log, floor 
+  
+  # we want to find $L$ such that ( max_L min_coarse_size*cfactor**L <= num_steps)
+  levels =  floor(log(num_steps/min_coarse_size,cfactor))+1 
+
+  if levels<1:
+    levels = 1
+  return levels
+# end compute levels
+
+
+# See
+# https://stackoverflow.com/questions/383565/how-to-iterate-over-a-list-repeating-each-element-in-python
+def duplicate(iterable,n):
+  """A generator that repeats each entry n times"""
+  for item in iterable:
+    for _ in range(n):
+      yield item
+
+def initialize_weights(dest_model, src_model):
+  # model's should have the same number of children
+  assert(len(list(dest_model.children()))==len(list(src_model.children())))
+
+  # loop over all the children, initializing the weights
+  for dest,src in zip(dest_model.children(),src_model.children()):
+    # do something special for layer parallel modules
+    if isinstance(dest,torchbraid.LayerParallel):
+      # both are layery parallel
+      assert(type(dest) is type(src))
+
+      for lp_dest,lp_src in zip(dest.children(),duplicate(src.children(),2)):
+        with torch.no_grad():
+          for d,s in zip(lp_dest.parameters(),lp_src.parameters()):
+            d.copy_(s)
+        
+    else:
+      with torch.no_grad():
+        for d,s in zip(dest.parameters(),src.parameters()):
+          d.copy_(s)
+
+def main():
+  # Training settings
+  parser = argparse.ArgumentParser(description='TORCHBRAID CIFAR10 Example')
+  parser.add_argument('--seed', type=int, default=1, metavar='S',
+                      help='random seed (default: 1)')
+  parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                      help='how many batches to wait before logging training status')
+
+  # artichtectural settings
+  parser.add_argument('--steps', type=int, default=4, metavar='N',
+                      help='Number of times steps in the resnet layer (default: 4)')
+  parser.add_argument('--channels', type=int, default=4, metavar='N',
+                      help='Number of channels in resnet layer (default: 4)')
+
+  # algorithmic settings (gradient descent and batching
+  parser.add_argument('--batch-size', type=int, default=50, metavar='N',
+                      help='input batch size for training (default: 50)')
+  parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                      help='number of epochs to train (default: 2)')
+  parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                      help='learning rate (default: 0.01)')
+
+  # algorithmic settings (parallel or serial)
+  parser.add_argument('--force-lp', action='store_true', default=False,
+                      help='Use layer parallel even if there is only 1 MPI rank')
+  parser.add_argument('--lp-levels', type=int, default=3, metavar='N',
+                      help='Layer parallel levels (default: 3)')
+  parser.add_argument('--lp-iters', type=int, default=2, metavar='N',
+                      help='Layer parallel iterations (default: 2)')
+  parser.add_argument('--lp-print', type=int, default=0, metavar='N',
+                      help='Layer parallel print level default: 0)')
+
+  rank  = MPI.COMM_WORLD.Get_rank()
+  procs = MPI.COMM_WORLD.Get_size()
+  args = parser.parse_args()
+
+  torch.manual_seed(torchbraid.utils.seed_from_rank(args.seed,rank))
+
+  if args.lp_levels==-1:
+    min_coarse_size = 7
+    args.lp_levels = compute_levels(args.steps,min_coarse_size,4)
+
+  if args.steps % procs!=0:
+    root_print(rank,'Steps must be an even multiple of the number of processors: %d %d' % (args.steps,procs) )
+    sys.exit(0)
+
+  ni_levels = 3
+  ni_rfactor = 2
+  if args.steps % ni_rfactor**(ni_levels-1) != 0:
+    root_print(rank,'Steps combined with the coarsest nested iteration level must be an even multiple: %d %d %d' % (args.steps,ni_rfactor,ni_levels-1))
+    sys.exit(0)
+
+  if args.steps / ni_rfactor**(ni_levels-1) % procs != 0:
+    root_print(rank,'Coarsest nested iteration must fit on the number of processors')
+    sys.exit(0)
+
+  transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
+  train_set = datasets.CIFAR10('./data', download=False,
+                                   transform=transform,train=True)
+  test_set  = datasets.CIFAR10('./data', download=False,
+                                   transform=transform,train=False)
+
+  # reduce the number of samples for faster execution
+  train_set = torch.utils.data.Subset(train_set,range(500))
+  test_set = torch.utils.data.Subset(test_set,range(100))
+
+  train_loader = torch.utils.data.DataLoader(train_set,
+                                             batch_size=args.batch_size, 
+                                             shuffle=True)
+  test_loader  = torch.utils.data.DataLoader(test_set,
+                                             batch_size=args.batch_size, 
+                                             shuffle=False)
+
+  ni_steps = [int(args.steps/(ni_rfactor**(ni_levels-i-1))) for i in range(ni_levels)]
+
+  root_print(rank,ni_steps)
+
+  root_print(rank,'Using ParallelNet')
+  model_previous = None
+  for steps in ni_steps:
+    root_print(rank,'------------------------ optimizing %d steps' % steps)
+
+    local_steps = int(steps/procs)
+    model = ParallelNet(channels=args.channels,
+                        local_steps=local_steps,
+                        max_levels=args.lp_levels,
+                        max_iters=args.lp_iters,
+                        print_level=args.lp_print)
+
+    # pass the weights along to the next iteration
+    if model_previous is not None:
+      initialize_weights(model, model_previous)
+      root_print(rank,'not_none')
+
+    # the idea of this object is to handle the parallel communication associated
+    # with layer parallel
+    compose = model.compose
+  
+    optimizer = optim.SGD(model.parameters(), lr=args.lr,momentum=0.9)
+  
+    epoch_times = []
+    test_times = []
+    for epoch in range(1, args.epochs + 1):
+      start_time = timer()
+      train(rank,args, model, train_loader, optimizer, epoch,compose)
+      end_time = timer()
+      epoch_times += [end_time-start_time]
+  
+      start_time = timer()
+      test(rank,model, test_loader,compose)
+      end_time = timer()
+      test_times += [end_time-start_time]
+ 
+    model_previous = model
+
+
+  root_print(rank,'TIME PER EPOCH: %.2e (1 std dev %.2e)' % (stats.mean(epoch_times),stats.stdev(epoch_times)))
+  root_print(rank,'TIME PER TEST:  %.2e (1 std dev %.2e)' % (stats.mean(test_times), stats.stdev(test_times)))
+
+if __name__ == '__main__':
+  main()
