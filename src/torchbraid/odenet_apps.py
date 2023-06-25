Fixing backpropped and the update of the done flag

src/torchbraid/odenet_apps.py
@@ -110,7 +110,6 @@ class ForwardODENetApp(BraidApp):
         self.done_flag = tb_utils.DoneFlag.allocate()
 
       def updateLayerDoneFlag(self,new_state):
-        print(f'UPDATE {new_state}')
         tb_utils.DoneFlag.update(self.done_flag,new_state)
 
       def registerLayerDoneFlag(self,layer):
@@ -464,7 +463,7 @@ class ForwardODENetApp(BraidApp):
         ny = layer(dt,t_y)
         y.replaceTensor(ny.detach().clone()) 
 
-      self.backpropped[tstart,tstop] = (t_y,ny)
+      self.backpropped[ts_index] = (t_y,ny)
     else:
       with torch.no_grad():
         ny = layer(dt,t_y)
@@ -493,8 +492,8 @@ class ForwardODENetApp(BraidApp):
       assert ts_index in self.layer_dict
       layer = self.layer_dict[ts_index]
 
-      if level==0 and (tstart,tstop) in self.backpropped:
-        x,y = self.backpropped[(tstart,tstop)]
+      if level==0 and ts_index in self.backpropped:
+        x,y = self.backpropped[ts_index]
         return (y,x), layer
     
     t_x = b_x.tensor()

src/torchbraid/utils/lp_batchnorm.py
@@ -34,21 +34,25 @@ class LPBatchNorm2d(DoneFlagMixin,nn.Module):
     self.register_buffer("var",var)
     self.register_buffer("done_flag",done_flag)
 
+  def reset_running_stats(self):
+    self.mean.zero_()
+    self.var.fill_(1)
+
   def forward(self,x):
     if self.done_flag and self.training:
       bn_train_mode = True
       mean = self.mean
       var = self.var
-      print('TRAIN-DONE')
+      #print('TRAIN-DONE')
     elif not self.done_flag and self.training:
       bn_train_mode = True
       mean = self.mean.clone()
       var = self.var.clone()
-      print('TRAIN-FIX')
+      #print('TRAIN-FIX')
     else:
       bn_train_mode = False
       mean = self.mean
       var = self.var
-      print('EVAL')
+      #print('EVAL')
  
     return F.batch_norm(x,mean,var,None,None,bn_train_mode,self.momentum,self.eps)

tests/test_layer_parallel.py
@@ -200,8 +200,6 @@ class TestTorchBraid(unittest.TestCase):
     except RuntimeError as err:
       raise RuntimeError("proc=%d) reLUNetBN_Approx..failure" % rank) from err
 
-    bn_parallel = [l for l in m_parallel.modules() if isinstance(l,nn.BatchNorm1d) or isinstance(l,LPBatchNorm2d)]
-    self.assertTrue(len(bn_parallel)>0)
     if m_serial!=None:
       bn_serial = [l for l in m_serial.modules() if isinstance(l,nn.BatchNorm1d) or isinstance(l,LPBatchNorm2d)]
       serial = ''
@@ -210,7 +208,10 @@ class TestTorchBraid(unittest.TestCase):
         serial += f'ser {ind:02d} {n}\n'
         
       print(serial)
+    # end serial
 
+    bn_parallel = [l for l in m_parallel.modules() if isinstance(l,nn.BatchNorm1d) or isinstance(l,LPBatchNorm2d)]
+    self.assertTrue(len(bn_parallel)>0)
     parallel = ''
     for ind,bn in enumerate(bn_parallel):
       n = [b for b in bn.buffers()]