03b7f3c44d7542fe305e40cd157041ace0374c5b
Eric C. Cyr <eccyr@sandia.gov>
2024-11-07 11:57:53 -0800

    Updating versions

--- setup.py
+++ setup.py
@@ -64,10 +64,10 @@ extension = [Extension(
 install_requires = [
   'setuptools',
   'mpi4py',
-  'cython==0.29.32',
+  'cython>=0.29.32',
   'numpy',
-  'torch==2.0.1',
-  'torchvision==0.15.2',
+  'torch>=2.0.1',
+  'torchvision>=0.15.2',
   'matplotlib'
 ]
 
