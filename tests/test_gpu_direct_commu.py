c90923dcef7c56cec40da6adbf85965eefac950a
Eric C. Cyr <eccyr@sandia.gov>
2024-12-12 11:49:02 -0700

    More controlled messaging for gpu aware mpi

--- tests/test_gpu_direct_commu.py
+++ tests/test_gpu_direct_commu.py
@@ -2,11 +2,11 @@ import torch
 import unittest
 from mpi4py import MPI
 
-class TestGpuDirect(unittest.TestCase):
-
-  def test_gpu_direct(self):
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
+def test_gpu_direct():
+  comm = MPI.COMM_WORLD
+  rank = comm.Get_rank()
+  passed = False 
+  try:
 
     ten = torch.ones(100, device='cuda') * (10+rank)
     ten2 = torch.empty(100, device='cuda')
@@ -25,10 +25,29 @@ class TestGpuDirect(unittest.TestCase):
       comm.Recv(ten2, source=0, tag=88)
       comm.Send(ten, dest=0, tag=89)
       torch.allclose(ten2, ten3)
+  except AssertionError as e:
+    err_msg = f'Assertion Failure - {e}'
+  else:
+    passed = True
 
+
+  if not passed:
+    print(f'FAILURE (p={rank}): {err_msg}')
+
+  comm.Barrier()
+
+
+  if rank==1:
+    comm.send(passed, dest=0, tag=90)
+  else:
+    passed_rank0 = passed
+    passed_rank1 = comm.recv(source=1, tag=90)
+
+    if passed_rank0 and passed_rank1:
+      print('PASSED: GPU aware MPI is availble')
+    else:
+      print()
+      print('FAILED: GPU aware MPI is not available')
 
 if __name__ == '__main__':
-  unittest.main()
-
-
-
+    test_gpu_direct()
